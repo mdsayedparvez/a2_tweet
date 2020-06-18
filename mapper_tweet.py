a5761339d4b306842c79ab40bf2a5a8f743273c7
mdsayedparvez <62695264+mdsayedparvez@users.noreply.github.com>
2020-06-18 23:45:50 +0200

    update final

--- mapper_tweet.py
+++ mapper_tweet.py
@@ -8,27 +8,23 @@ def validateJSON(jsonData):
     except ValueError as err:
         return False
     return True
-word_list = ["han", "hon", "den", "det", "denna", "denne","hen"]
+list_of_word = ["han", "hon", "den", "det", "denna", "denne","hen"]
 count = 0
 for line in fileinput.input():
     if not line.strip():
         continue
     tweets = line.split("^M")
     for tweet in tweets:
-        #json_data = json.loads(tweet)
-        isValid = validateJSON(tweet)
-        #json_data = json.loads(tweet)
-        if (isValid == True):
+             isValid = validateJSON(tweet)
+                if (isValid == True):
             json_data = json.loads(tweet)
-	     #print (json_data["retweeted"])
-            if (json_data["retweeted"] == False):
+	     if (json_data["retweeted"] == False):
                 count = count + 1
-                #print (json_data["text"])
-                tweet_words = json_data["text"].split()
-                for search_word in word_list:
+               tweet_words = json_data["text"].split()
+                for search_word in list_of_word:
                     for tweet_word in tweet_words:
                         if (search_word == tweet_word.lower()):
-                            print (("number of tweets mentioning pronoun %s\t%s") % (search_word,1))
+                            print (("Counting the number of tweets mentioning each pronoun %s\t%s") % (search_word,1))
                             break
 
 
